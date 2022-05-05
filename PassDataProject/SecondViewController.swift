17cd76257aa75e1f92cb2e434a64973668867cb4
Matvei95 <khlestov.95@mail.ru>
2022-05-05 23:03:24 +0500

    Working with code. Segue and Unwind segue.

---
+++ PassDataProject/SecondViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SecondViewController.swift
+//  PassDataProject
+//
+//  Created by Matvei Khlestov on 05.05.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    var login: String?
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        button.layer.cornerRadius = 10
+        
+        guard let login = self.login else {return}
+        label.text = "Hello, \(login)"
+
+    }
+    
+    @IBAction func goBackTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "unwindSegue", sender: nil)
+    }
+    
+    
+    }
+    
+
--- PassDataProject/ViewController.swift
+++ PassDataProject/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var labelResult: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loginButton.layer.cornerRadius = 10
+        
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let destinationVC = segue.destination as? SecondViewController else {return}
+        destinationVC.login = loginTF.text
+    }
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "detailSegue", sender: nil)
+        
+    }
+    
+    @IBAction func unwindSegueToMainScreen(segue: UIStoryboardSegue) {
+        guard segue.identifier == "unwindSegue" else { return }
+        guard let sourceVC = segue.source as? SecondViewController else { return }
+        self.labelResult.text = sourceVC.label.text
+    }
+    
 
 }
 
